c1a63ad43474456479a62f18ed82a0566bf50370
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-11 16:42:42 +0000

    Added Account with classes

--- php/Account.php
+++ php/Account.php
@@ -3,5 +3,13 @@
 
 class Account
 {
-
+    public $picture;
+    public $ID;
+    public $username;
+    public function setID($ID){
+        $this->ID = $ID;
+    }
+    public function getID(){
+        return $this->ID;
+    }
 }
