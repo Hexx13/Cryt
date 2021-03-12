6e7c2e048f44925f8e82ad16008524436d1a59cb
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-12 15:15:14 +0000

    Created get/set for profilePicturePath

--- php/Customer.php
+++ php/Customer.php
@@ -8,4 +8,12 @@ class Customer extends Account
     private $firstName;
     private $lastName;
     private $wallet;
+    public function getProfilePicturePath()
+    {
+        return $this->profilePicturePath;
+    }
+    public function setProfilePicturePath($profilePicturePath)
+    {
+        $this->profilePicturePath = $profilePicturePath;
+    }
 }
