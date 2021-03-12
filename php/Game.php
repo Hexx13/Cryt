c1105920c487b0775b21fa28488e6b9a7e15d556
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-12 15:04:12 +0000

    Made variables private

--- php/Game.php
+++ php/Game.php
@@ -3,12 +3,12 @@
 
 class Game
 {
-    public $gameID;
-    public $name;
-    public $description;
-    public $price;
-    public $sale;
-    public $filePath;
+    private $gameID;
+    private $name;
+    private $description;
+    private $price;
+    private $sale;
+    private $filePath;
 
     public function getGameID(){
         return $this->gameID;
@@ -29,5 +29,13 @@ class Game
     public function setDescription($description)
     {
         $this->description = $description;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function setPrice($price)
+    {
+        $this->price = $price;
     }
 }
