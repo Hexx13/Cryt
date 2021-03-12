972d941bbdd89d3c77d5fddb9fff315848b9c03d
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-12 15:00:54 +0000

    Created get/set for name and description

--- php/Game.php
+++ php/Game.php
@@ -13,4 +13,21 @@ class Game
     public function getGameID(){
         return $this->gameID;
     }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
 }
