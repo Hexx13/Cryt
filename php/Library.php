8390ea80c3247d24a4aba7bab87e3b37d0ee91d9
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-13 16:57:18 +0000

    Deleted everything in Library

--- php/Library.php
+++ php/Library.php
@@ -4,21 +4,4 @@
 class Library
 {
 
-    public $gameList = array();
-    public $inLibrary;
-    public $playAdd;
-    public $selectAdd;
-    public $downloadAdd;
-
-    //function takes a Game object, adds it to array on the next available index in the array
-    public function addGameList($game)
-    {
-        $this->gameList[$this->gameList.length] = $game;
-    }
-    public function addToLibrary($gamelist){
-        foreach ($gamelist as $game){
-            // add game to database library of user
-        }
-    }
-    public function getGameList(){}
 }
