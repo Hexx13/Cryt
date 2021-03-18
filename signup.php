4be83ad8515952f8e6da54e8cd3967fd0aea6b05
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-18 14:40:39 +0000

    Still trying to figure this out

---
+++ signup.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cryt</title>
+</head>
+<body>
+
+<main>
+    <form action="php/">
+    </form>
+
+</main>
+
+</body>
+</html>
