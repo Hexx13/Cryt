e00d3d0698e7673b0a5909e2177b6a96cf3183a8
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-18 15:22:48 +0000

    Changes

--- signup.php
+++ signup.php
@@ -7,7 +7,24 @@
 <body>
 
 <main>
-    <form action="php/">
+    <form action="php/Customer.php" method="createAccountDB">
+
+        <label for="username">Username: </label>
+        <input type="text" name="username"><br>
+
+        <label for="firstName">First name:</label>
+        <input type="text" name="firstName"><br>
+
+        <label for="lastName">Last name:</label>
+        <input type="text" name="lastName"><br>
+
+        <label for="email">Email: </label>
+        <input type="email" name="email"><br>
+
+        <label for="password">Password: </label>
+        <input type="password" id="password" name="password"><br>
+
+        <input type="submit" value="Submit">
     </form>
 
 </main>
