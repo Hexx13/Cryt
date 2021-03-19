1b0dc554e4cfe254d7b6eda78922e885a444c6c7
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-19 14:40:53 +0000

    Made form for TopUp.php

--- TopUp.php
+++ TopUp.php
@@ -1,1 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cryt</title>
+</head>
+<body>
+
+<main>
+    <form action="php/TopUp.php" method="addAmount">
+
+        <label for="amount">Enter Amount: </label>
+        <input type="text" name="amount"><br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+</main>
+
+</body>
+</html>
+
