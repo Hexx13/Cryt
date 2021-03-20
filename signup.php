3ab21bcd497ae1196e843f31ebdc8635349316e1
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-20 16:12:38 +0000

    Created multiple pages and added template to these files

--- signup.php
+++ signup.php
@@ -5,6 +5,31 @@
     <title>Cryt</title>
 </head>
 <body>
+<div class="header">
+    <div class="leftHead">
+        <div class="logoBox"></div>
+        <div class="headerLinks">
+            <a href="Index.html">Home</a>
+            <a href="Index.html">Store</a>
+            <a href="Index.html">Library</a>
+        </div>
+    </div>
+
+    <div class="headSpacer"></div>
+    <div class="rightHead">
+        <div class="balanceBox">
+            <a class="balanceLink">
+                Balance:
+                <?php
+                echo "3.00"
+                ?> €
+            </a>
+        </div>
+        <div class="sign-upBox"></div>
+        <div class="profPicBox"></div>
+    </div>
+</div>
+
 
 <main>
     <form action="php/Customer.php" method="post">
