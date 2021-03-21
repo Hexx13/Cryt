fbf2162423c617246a404af869fe0d521230da5e
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-21 19:35:27 +0000

    Requires in index

--- index.php
+++ index.php
@@ -5,30 +5,9 @@
     <title>Cryt</title>
 </head>
 <body>
-<div class="header">
-    <div class="leftHead">
-        <div class="logoBox"></div>
-        <div class="headerLinks">
-            <a href="Index.html">Home</a>
-            <a href="Index.html">Store</a>
-            <a href="Index.html">Library</a>
-        </div>
-    </div>
-
-    <div class="headSpacer"></div>
-    <div class="rightHead">
-        <div class="balanceBox">
-            <a class="balanceLink">
-                Balance:
-                <?php
-                echo "3.00"
-                ?> €
-            </a>
-        </div>
-        <div class="sign-upBox"></div>
-        <div class="profPicBox"></div>
-    </div>
-</div>
+<?php
+require 'php/Layout/Header.php';
+?>
 <main>
 
     <div class="mainSideSpacer"></div>
@@ -45,32 +24,8 @@
 
 
 </main>
-<footer class="footer">
-    <div class="footLeft">
-        <div class="footLeftTop">
-            <a>Contact Us</a>
-            <a>Upload your Game</a>
-            <a>Join our team</a>
-            <a>Blog</a>
-        </div>
-        <div class="footLeftBot">
-            <div>
-                <a>Legal</a>
-                <a>Privacy policy</a>
-                <a>Cookies</a>
-            </div>
-            <div>
-                <p>Blah Blah Copyright shite lorem ipsum dolor your ma is nice</p>
-            </div>
-        </div>
-    </div>
-    <div class="footRight">
-        <a><img src=""></a>
-        <a><img src=""></a>
-        <a><img src=""></a>
-        <a><img src=""></a>
-    </div>
-
-</footer>
+<?php
+require 'php/Layout/Footer.php';
+?>
 </body>
 </html>
