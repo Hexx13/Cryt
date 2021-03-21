57dec9c7b4625f89ec2e72c110f552e364f25900
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-21 16:41:14 +0000

    we do a bit of trolin

--- signup.php
+++ signup.php
@@ -41,7 +41,7 @@
         <input type="text" name="firstName"><br>
 
         <label for="lastName">Last name:</label>
-        <input type="text" name="lastName">
+        <input type="text" name="lastName"><br>
 
 
         <label for="email">Email: </label>
