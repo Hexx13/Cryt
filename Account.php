e45f45bbe53d652bf6b41c301a5477a0123169f2
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-21 19:38:09 +0000

    Layout in Account doned

--- Account.php
+++ Account.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cryt</title>
+</head>
+<body>
 <?php
+require 'php/Layout/Header.php';
+?>
+<main>
+
+
+</main>
+<?php
+require 'php/Layout/Footer.php';
+?>
+</body>
+</html>
+
