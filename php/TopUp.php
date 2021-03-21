b51764d41689d4d4e80a6b48b702da1ea8da03bb
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-21 21:01:42 +0000

    Update TopUp.php

--- php/TopUp.php
+++ php/TopUp.php
@@ -4,6 +4,37 @@
 class TopUp
 {
     public $amount;
+   public $wallet_ID;
+    public function __construct($amount, $wallet_ID)
+    {
+        $this->amount = $amount;
+        $this->wallet_ID=$wallet_ID;
+    }
+
+    public function getBalance()
+    {
+        //make connection
+        $link = mysqli_connect("localhost",
+            "root",
+            "password",
+            "mydb");
+
+        // Check connection
+        if ($link === false) {
+            die("CANNOT CONNECT OH NO OH NO"
+                . mysqli_connect_error());
+        }
+
+        $balanceQuery = "SELECT wallet_Balance from wallet WHERE wallet_ID= $this->walletID;";
+
+        if($result = $link ->query($balanceQuery)){
+            while ($fieldinfo  = $result ->fetch_field()){
+                $curBal = $fieldinfo;
+            }
+            return $curBal;
+        }
+    }
+
     public function setAmount($amount)
     {
         $this->amount = $amount;
