dc10d873823c9868685983461259180baf036056
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-21 16:36:24 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -40,8 +40,10 @@
         <label for="firstName">First name:</label>
         <input type="text" name="firstName"><br>
 
-        <label for="lastName">Last name:</label>
-        <input type="text" name="lastName"><br>
+        <!--<label for="lastName">Last name:</label>-->
+        <label>
+            <input type="text" name="lastName">
+        </label><br>
 
         <label for="email">Email: </label>
         <input type="email" name="email"><br>
