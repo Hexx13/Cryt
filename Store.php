f9b7f9717b07588fec4b9949a125fc64fce875f4
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-03-21 17:46:59 +0000

    Html in Store.php

--- Store.php
+++ Store.php
@@ -8,8 +8,42 @@ require 'php/Layout/Header.php';
 
 <main>
 
-</main>
+    <?php
+    $game = array(
+            'name' => 'beans',
+            'price' => 'pog beans',
+            'about' => 'Egins beans',
+    );
+    ?>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <h1>Store Page</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class = "game">
+                <h1><?php echo $game['name']; ?></h1>
+                <h2><?php echo $game['price']; ?></h2>
+                <p><?php echo $game['about'] ?></p>
+            </div>
+            <?php
+            foreach ($game as $gaming) {
+                echo '<div class="col-lg-4">';
+                echo '<h2>';
+                echo $gaming;
+                echo '</h2>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </div>
+
+</main>
 <?php
 require 'php/Layout/Footer.php';
 ?>
