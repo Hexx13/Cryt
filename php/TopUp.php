87b59a0290d73626a971025ce964e9fca21c6c64
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-03-25 15:07:11 +0000

    stufff

--- php/TopUp.php
+++ php/TopUp.php
@@ -11,29 +11,7 @@ class TopUp
         $this->wallet_ID=$wallet_ID;
     }
 
-    public function getBalance()
-    {
-        //make connection
-        $link = mysqli_connect("localhost",
-            "root",
-            "password",
-            "mydb");
 
-        // Check connection
-        if ($link === false) {
-            die("CANNOT CONNECT OH NO OH NO"
-                . mysqli_connect_error());
-        }
-
-        $balanceQuery = "SELECT wallet_Balance from wallet WHERE wallet_ID= $this->walletID;";
-
-        if($result = $link ->query($balanceQuery)){
-            while ($fieldinfo  = $result ->fetch_field()){
-                $curBal = $fieldinfo;
-            }
-            return $curBal;
-        }
-    }
 
     public function setAmount($amount)
     {
--- php/Wallet.php
+++ php/Wallet.php
@@ -8,8 +8,28 @@ class Wallet
 
     public function getBalance()
     {
-        return $this->balance;
+        //make connection
+        $link = mysqli_connect("localhost",
+            "root",
+            "password",
+            "mydb");
+
+        // Check connection
+        if ($link === false) {
+            die("CANNOT CONNECT OH NO OH NO"
+                . mysqli_connect_error());
+        }
+
+        $balanceQuery = "SELECT wallet_Balance from wallet WHERE wallet_ID= $this->walletID;";
+
+        if($result = $link ->query($balanceQuery)){
+            while ($fieldinfo  = $result ->fetch_field()){
+                $curBal = $fieldinfo;
+            }
+            return $curBal;
+        }
     }
+
     public function setBalance($balance)
     {
         $this->balance = $balance;
