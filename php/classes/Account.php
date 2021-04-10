25422704fa6b7b41003757bfe28307080577a21f
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-10 21:19:52 +0100

    Un-spaghettified the log in page

--- php/classes/Account.php
+++ php/classes/Account.php
@@ -7,9 +7,31 @@ class Account
     protected $username;
     protected $password;
 
-    public function loginAccount($username, $password){
+    public function loginQuery($username, $password){
+        //make connection
+        include_once "php/classes/Database.php";
+        $db = new Database();
+        $link = $db->getLink();
+
+        $sql = "select * from account where account_username='$username'and account_Password='$password'";
+        if ($res = mysqli_query($link, $sql)){
+            return mysqli_fetch_array($res);
+        }else{
+            echo "ERROR: OH GOD OH NO WHY aaaaaaaaaaaaaaaaaaaaa $sql. "
+                . mysqli_error($link);
+        }
 
     }
+
+    public function login($result){
+        if ($result) {
+            $_SESSION["login"] = "1";
+            header("location:index.php");
+        } else {
+            header("location:login.php?err=1");
+        }
+    }
+
 
     public function getAccountID()
     {
