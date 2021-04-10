52c53854b079068678a6f4b73a24d318a5120a44
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-10 15:16:22 +0100

    Fixed log-in, can now log in but still no error message when inputting wrong details.

--- Login.php
+++ Login.php
@@ -8,7 +8,7 @@
 <body>
 
 <main>
-    <form action="Login.php" method="post">
+    <form action="Login.php" method="POST">
 
         <label for="username">Username: </label>
         <input type="text" name="username"><br>
@@ -16,7 +16,7 @@
         <label for="password">Password: </label>
         <input type="password" id="password" name="password"><br>
 
-        <input type="submit" value="Login" name="sub">
+        <input type="submit" value="Login" name="submit">
     </form>
 
     <?php
@@ -27,30 +27,44 @@
         <?php if (isset($message)) {
             echo $message;
         } ?>
-
     </p>
     <?php
     if (isset($_REQUEST['submit'])) {
+
         $usname = $_REQUEST['username'];
         $uspassword = $_REQUEST['password'];
 
+        //make connection
+        include_once "php/classes/Database.php";
         $db = new Database();
         $link = $db->getLink();
 
-        $sql = "select* from users where account_username='$usname'and account_Password='$uspassword'";
+        $sql = "select * from account where account_username='$usname'and account_Password='$uspassword'";
 
-        $res = mysqli_query($link, $sql);
-        $result = mysqli_fetch_array($res);
-        if ($result) {
+        if ($res = mysqli_query($link, $sql)){
+            $result = mysqli_fetch_array($res);
+            if ($result) {
+                $_SESSION["login"] = "1";
+                header("location:index.php");
+            } else {
 
-            $_SESSION["login"] = "1";
-            header("location:index.php");
-        } else {
-            header("location:login.php?err=1");
-
-        }
-    }
+                if (isset($_REQUEST["err"]))
+                    $msg = "Invalid Login Details";
     ?>
+                <p style="color:red;">
+                    <?php if (isset($message)) {
+                        echo $message;
+                    }
+                }
+            } else {
+                echo "ERROR: OH GOD OH NO WHY aaaaaaaaaaaaaaaaaaaaa $sql. "
+                    . mysqli_error($link);
+            }
+        }
+
+        ?>
+
+    </p>
 </main>
 
 </body>
