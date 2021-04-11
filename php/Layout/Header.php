7316305bc27769dff2b14979ec932bf750a47537
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-11 17:19:12 +0100

    Header styles, and logo added to Header.php

--- php/Layout/Header.php
+++ php/Layout/Header.php
@@ -4,14 +4,16 @@ include "php/classes/Wallet.php";
 $beans = new Wallet();
 ?>
 <style>@import "../../Stylesheets/stylesheet.css";</style>
-<div class="header">
-    <a href="#default" class="logo">Logo go here</a>
-    <div class="header-right">
+<div class="header" style="padding: 0;">
+
+    <div class="header-left">
+        <img src="img/logo.png" class="logo">
         <a class="active" href="Index.php">Home</a>
         <a href="Index.php">Store</a>
         <a href="Index.php">Library</a>
     </div>
-    <div class="topup">
+    <div class="spacer"></div>
+    <div class="header-right">
         <a href="../../TopUpPage.php">  Balance:
             <?php
             include_once "php/classes/Account.php";
