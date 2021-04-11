1c63bd10d66f1bcbbad98fe35e779cf15963ed70
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-11 19:43:04 +0100

    Removing redundant code

--- php/classes/Store.php
+++ php/classes/Store.php
@@ -5,7 +5,9 @@ class Store
 {
 
     public function getAllGamesTable(){
-
+        include_once "php/classes/Database.php";
+        $db = new Database();
+        $link = $db->getLink();
     }
 
     public function addGame($game){
