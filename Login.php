ac32406fd5f960d252bed63684f89cc5d87d5847
JB <B00128828@mytudublin.ie>
2021-04-11 21:56:21 +0100

    more form style

--- Login.php
+++ Login.php
@@ -2,12 +2,14 @@
 <html lang="en">
 <?php session_start(); ?>
 <head>
+    <link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css"/>
     <meta charset="UTF-8">
     <title>Cryt</title>
 </head>
 <body>
 
 <main>
+    <div class="backgroundGrad">
     <form action="Login.php" method="POST">
 
         <label for="username">Username: </label>
@@ -18,7 +20,7 @@
 
         <input type="submit" value="Login" name="submit">
     </form>
-
+    </div>
     <?php
     if(isset($_REQUEST["err"]))
         $msg="Invalid username or Password";
