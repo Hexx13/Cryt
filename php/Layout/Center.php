ba97f1b19afca230ad6b0190535f6e53594c49e0
JB <B00128828@mytudublin.ie>
2021-04-11 20:16:43 +0100

    AAA

--- php/Layout/Center.php
+++ php/Layout/Center.php
@@ -1,1 +1,12 @@
-<?php
+<html>
+<head><style>
+
+
+        @import "../../Stylesheets/stylesheet.css";
+        #grad1 {
+            height: 100%;
+            background-color: black; /* For browsers that do not support gradients */
+            background-image: linear-gradient(rgba(27,27,51,0), rgba(13,10,31,0));
+        }</style></head>
+<main>
+
