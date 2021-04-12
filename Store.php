04c0ec8ce1a009f6ac35350516bc3d7c02bad0fb
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-04-12 12:40:58 +0100

    doing game things/new form

--- Store.php
+++ Store.php
@@ -23,56 +23,8 @@ require 'php/Layout/Header.php';
         </div>
     </div>
 
-   <!-- <div class="container2">
-        <div class="row2">
-            <div class = "game">
-                <h1><?php /* echo $game['name']; */?></h1>
-                <h2><?php /*echo $game['price']; */?></h2>
-                <p><?php /*echo $game['about'] ?>*/ ?>
-            </div>
-        </div>
-    </div> -->
 
-    <form action="signup.php" method="post">
 
-        <label for="gameName">Game Name: </label>
-        <input type="text" name="gameName"><br>
-
-        <label for="gameDesc">Game Description:</label>
-        <input type="text" name="gameDesc"><br>
-
-        <label for="gamePrice">Game Price:</label>
-        <input type="text" name="gamePrice"><br>
-
-        <label for="gameFilePath">Game File Path: </label>
-        <input type="text" name="gameFilePath"><br>
-
-        <label for="gameAuthor">Game Author: </label>
-        <input type="text" name="gameAuthor"><br>
-
-        <label for="gameID">Game ID: </label>
-        <input type="text" name="GameID"><br>
-
-        <input type="submit" value="Submit">
-    </form>
-
-    <div>
-        <?php
-
-        include "php/classes/Customer.php";
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username =  $_REQUEST['username'];
-            $password = $_REQUEST['password'];
-            $emailAddress = $_REQUEST['email'];
-            $firstName = $_REQUEST['firstName'];
-            $lastName = $_REQUEST['lastName'];
-
-            $customer = new Customer($password,$username, $emailAddress,$firstName,$lastName);
-            $customer->createAccountDB();
-            header("location:login.php");
-        }
-        ?>
 
 
 
