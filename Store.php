85b03fd0962d3654bf7ee63834ffb2dbd6e23b90
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-04-12 13:55:12 +0100

    general store housekeeping

--- Store.php
+++ Store.php
@@ -1,42 +1,32 @@
 <!DOCTYPE html>
 <?php
 include_once "php/classes/Game.php";
-session_start ();
-if(!isset($_SESSION["login"]))
+session_start();
+if (!isset($_SESSION["login"]))
 
     header("location:login.php");
 ?>
 <html lang="en">
 <link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css"/>
-<?php
-require 'php/Layout/Header.php';
-?>
-
 
 <main class="backgroundGrad">
 
+    <?php
+    require 'php/Layout/Header.php';
+    ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-6">
-                <h1>Store Page</h1>
-            </div>
-        </div>
-    </div>
+    <?php
+    $gamers = new Game();
 
-
-<?php
-$gamers = new Game();
-
-var_dump($gamers->getGameArray()); ?>
+    var_dump($gamers->getGameArray());
+    ?>
 
 
-
-
+    <?php
+    require 'php/Layout/Footer.php';
+    ?>
 
 </main>
-<?php
-require 'php/Layout/Footer.php';
-?>
+
 </html>
 
