851dd5010128730150e818d5847af346b499c3d5
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-04-12 14:48:14 +0100

    Store page basic heading for games

--- Store.php
+++ Store.php
@@ -13,13 +13,40 @@ if (!isset($_SESSION["login"]))
 
     <?php
     require 'php/Layout/Header.php';
-    ?>
 
-    <?php
     $gamers = new Game();
 
-    var_dump($gamers->getGameArray());
+    $gameArray = $gamers->getGameArray();
+
     ?>
+
+    <div>
+        <div>
+        <?PHP foreach($gameArray as $game){ ?>
+            <div>
+                <h2><?PHP echo $game['game_Name']?></h2>
+
+                <div>
+                <?PHP
+                echo $game['game_Desc'];
+                echo "<br>";
+                echo $game['game_Price'];
+                echo "<br>";
+                echo $game['game_File_Path'];
+                echo "<br>";
+                echo "<br>";
+                ?>
+                </div>
+                <!-- add path for images for ez shane slideshow -->
+
+            </div>
+
+
+        </div>
+        <?PHP } ?>
+
+
+    </div>
 
 
     <?php
@@ -29,4 +56,3 @@ if (!isset($_SESSION["login"]))
 </main>
 
 </html>
-
