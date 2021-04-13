09ac43088335cc1eff06287d8a00135fc65526e1
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-04-13 20:34:36 +0100

    index image

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ if(!isset($_SESSION["login"]))
 <html lang="en">
 <head>
     <link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css"/>
-    <script type="text/javascript" src="JavaScript/slideshow.js"></script>
     <meta charset="UTF-8">
     <title>Cryt</title>
 </head>
@@ -18,29 +17,10 @@ if(!isset($_SESSION["login"]))
     <?php
     require 'php/Layout/Header.php';
     ?>
-
-    <div class="slideshow-container">
-
-        <div class="mySlides fade">
-            <img src="img/default_pfp.png" alt="" style="width:100%">
+        <div class="imageIndex">
+            <img src="img/logo.png" class="imageIndex">
         </div>
 
-        <div class="mySlides fade">
-            <img src="img/logo.png" alt= "" style="width:100%">
-        </div>
-
-        <div class="mySlides fade">
-            <img src="" alt="" style="width:100%">
-        </div>
-
-    </div>
-    <br>
-
-    <div style="text-align:center">
-        <span class="dot"></span>
-        <span class="dot"></span>
-        <span class="dot"></span>
-    </div>
 
         <?php
         require 'php/Layout/Footer.php';
