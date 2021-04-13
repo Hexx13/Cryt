00d5046f52cd7d41101aaf3ad0b2a091e13fdd29
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-04-13 20:17:39 +0100

    remoce lL

--- index.php
+++ index.php
@@ -18,29 +18,7 @@ if(!isset($_SESSION["login"]))
     <?php
     require 'php/Layout/Header.php';
     ?>
-    <script>
-        let slideIndex = 0;
-        slidey();
-        function slidey(){
-            let i;
-            let dots = document.getElementsByClassName("dot");
-            let sliding = document.getElementsByClassName("mySlides");
-            for(i = 0; i < sliding.length; i++){
-                sliding[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > sliding){
-                slideIndex = 1;
-            }
-            for(i = 0; i < dots.length; i++){
-                dots[i].className = dots[i].className.replace(" active2", "");
-            }
-            sliding[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active2";
-            setTimeout(slidey, 5000);
-        }
 
-    </script>
     <div class="slideshow-container">
 
         <div class="mySlides fade">
