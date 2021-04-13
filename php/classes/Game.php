a8817c87cf3f2cb16f21264912e56be7e9b684a6
Egino200 <71835027+Egino200@users.noreply.github.com>
2021-04-13 21:25:59 +0100

    Update Game.php

--- php/classes/Game.php
+++ php/classes/Game.php
@@ -35,4 +35,20 @@ class Game
         }
     }
 
+    public function getGame($gameID)
+    {
+        //make connection
+        include_once "php/classes/Database.php";
+        $db = new Database();
+        $link = $db->getLink();
+
+        $sql = "select * from game where game_ID  = $gameID";
+
+        if ($res = mysqli_query($link, $sql)) {
+            return mysqli_fetch_assoc($res);
+
+        } else {
+            echo "ERROR: OH JOD OH NO WHY XAXAXAXAXXAXAXA $sql ." . mysqli_error($link);
+        }
+    }
 }
