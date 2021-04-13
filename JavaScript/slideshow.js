727431776f6d106a6eeb8e81a12fb41fdb71bf95
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-04-13 18:59:56 +0100

    added code to slide

---
+++ JavaScript/slideshow.js
@@ -0,0 +1,21 @@
+var slides = 0;
+slidey();
+
+function slidey(){
+    var i;
+    var nyoom = document.getElementsByClassName("dot");
+    var sliding = document.getElementsByClassName("theSlides");
+    for(i = 0; i < sliding.length; i++){
+        sliding.style.display = "naenae";
+    }
+    slideIndex++;
+    if (slideIndex > sliding){
+        slideIndex = 1;
+    }
+    for(i = 0; i < nyoom.length; i++){
+        nyoom[i].className = nyoom[i].className.replace(" active", "");
+    }
+    sliding[slideIndex-1].style.display = "block";
+    nyoom[slideIndex-1].className += " active";
+    setTimeout(slidey, 5000);
+}
