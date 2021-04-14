129d0529735711449fd69b6ffe6f49bcc0ef1579
Orbitz4k <72084883+Orbitz4k@users.noreply.github.com>
2021-04-14 16:56:49 +0100

    added stuff to Library.php

--- Library.php
+++ Library.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
 <?php
+session_start ();
+if(!isset($_SESSION["login"]))
+    header("location:login.php");
+?>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css"/>
+    <meta charset="UTF-8">
+    <title>Cryt</title>
+</head>
+<body class="backgroundGrad">
+
+<?php
+require 'php/Layout/Header.php';
+?>
+
+
+<?php require 'php/Layout/Footer.php'; ?>
+
+
+</body>
+</html>
