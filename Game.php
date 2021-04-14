13674f0ac09da0fc1c7785b812e4138c80489793
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-14 14:03:26 +0100

    Added Game class to game page

--- Game.php
+++ Game.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
+<?php session_start(); ?>
+<head>
+    <link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css"/>
+    <meta charset="UTF-8">
+    <title>Cryt</title>
+</head>
 <body class="backgroundGrad">
     <?php
     require 'php/Layout/Header.php';
+    include_once "php/classes/Game.php";
+    $gamers = new Game();
+    $gameArray = $gamers->getGame();
     ?>
+
+    <div>
+        <div>
+            <h1>
+                <?Php>
+            </h1>
+        </div>
+    </div>
+
 
     <?php
     require 'php/Layout/Footer.php';
