79bc1e31b9deb6059a7af3e8d3eaef8b14d8f67c
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-14 22:35:22 +0100

    Added stuff currentdate function to Transaction.php

--- php/classes/Transaction.php
+++ php/classes/Transaction.php
@@ -6,6 +6,9 @@ class Transaction
 
 
     public function transact($amount, $accountID){
+        $currentDate = new date();
+        include_once ("Customer.php");
+        $datetime = $currentDate->format('Y-m-d H:i:s');
 
     }
 }
