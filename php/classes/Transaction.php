9e50e4da38ed4e05b0efa50dd3eb9dde732aded9
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-14 22:41:13 +0100

    Added customer to transact function

--- php/classes/Transaction.php
+++ php/classes/Transaction.php
@@ -7,8 +7,11 @@ class Transaction
 
     public function transact($amount, $accountID){
         $currentDate = new date();
-        include_once ("Customer.php");
         $datetime = $currentDate->format('Y-m-d H:i:s');
+
+        include_once ("Customer.php");
+        $customer = new Customer();
+        $custArray = $customer->getAccountDetails();
 
 
     }
