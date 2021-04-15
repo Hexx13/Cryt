5a35ea223198497468a7b393db0a68627003a1c1
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-15 02:10:33 +0100

    added database link to transaaction class

--- php/classes/Transaction.php
+++ php/classes/Transaction.php
@@ -26,5 +26,11 @@ class Transaction
     public function transaction($transactionData,$amount, $accountID){
         $dataArray = $transactionData($amount, $accountID);
         $sql="insert into transaction (transaction_ID, dateTime, transaction_Amount, Wallet_wallet_ID) values ($dataArray[0], $dataArray[1], $dataArray[2])";
+
+        //make connection
+        include_once "php/classes/Database.php";
+        $db = new Database();
+        $link = $db->getLink();
+
     }
 }
